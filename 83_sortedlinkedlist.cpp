57cdb68c701f6e3b369e7b333d1fc4da84763513
Ayush Raj <161280074+ascendantayush@users.noreply.github.com>
2025-02-27 23:40:41 +0530

    Create 83_sortedlinkedlist.cpp

---
+++ 83_sortedlinkedlist.cpp
@@ -0,0 +1,31 @@
+// 83. remove duplicated from a sorted linked list
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head == nullptr) return head;
+
+        ListNode* temp = head;
+        while(temp->next != nullptr){
+            if(temp->next->val == temp->val){
+                temp->next = temp->next->next;
+            }else{
+                temp = temp->next;
+            }
+        }
+
+        return head;
+    }
+};
